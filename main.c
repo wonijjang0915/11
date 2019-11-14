64c86075322908d3480362c3cc5f9c3b9f48a922
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-11-14 15:30:52 +0900

    4

--- main.c
+++ main.c
@@ -4,17 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char buffer[8];
-	double *pd;
-	int *pi;
+	int i = 100;
+	int *p = &i;
+	int **q = &p;
 	
-	pd = (double *)buffer;
-	*pd = 3.14;
-	printf("%f\n", *(double*)buffer);
+	*p = 200;
+	printf("i=%d, *p=%d, **q=%d\n", i, *p, **q);
 	
-	pi = (int *)buffer;
-	*pi = 123;
-	*(pi+1) = 456;
-	printf("%d %d\n", *(int*)buffer, *((int*)buffer+1));
+	**q = 300;
+	printf("i=%d, *p=%d,**q=%d\n", i, *p, **q);
+	
 	return 0;
 }
